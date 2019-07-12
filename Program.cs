2ce56014e308c770ed996e64945d506548851cbd
dek-haji <deeeqhaji@gmail.com>
2019-07-12 09:19:02 -0500

    finished part one of the assignment

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Planner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Building FiveOneTwoEigth = new Building("512 8th Avenue");
+            Building NSS = new Building("500 interstate blvd");
+            FiveOneTwoEigth.Width = 13;
+            FiveOneTwoEigth.Depth = 10;
+            FiveOneTwoEigth.Stories = 124;
+            FiveOneTwoEigth.Construct();
+            
+
+            NSS.Width = 123;
+            NSS.Depth = 100;
+            NSS.Stories = 12;
+            NSS.Construct();
+            FiveOneTwoEigth.Purchase("dek");
+            NSS.Purchase("Adam");
+
+            FiveOneTwoEigth.PrintInfo();
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# urban-planer1
